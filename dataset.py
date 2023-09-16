ecd93449c0fc7b84a6c57ab184b223d08286521b
Muhammad Tarek <mohammadtarek000@gmail.com>
2023-09-16 19:41:45 +0300

    vggnet added

--- dataset.py
+++ dataset.py
@@ -65,7 +65,7 @@ train_transform = Compose(
 
 test_transform = Compose(
     [
-        Resize((227, 227)),
+        Resize((224, 224)),
         ToTensor(),
         Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
